1a8fa90e92eabfcf7d4721f55750996b694c22b8
Stephen Tillman <mrtillman.0001@gmail.com>
2021-04-16 18:03:36 -0400

    correct logout url

--- isAutenticated.js
+++ isAutenticated.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
   if (req.user) { return next(); }
   req.session.returnTo = req.originalUrl;
-  res.redirect('/login');
+  res.redirect('/auth/login');
 };
